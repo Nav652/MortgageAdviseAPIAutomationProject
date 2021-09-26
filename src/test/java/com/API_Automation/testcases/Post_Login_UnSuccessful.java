dc053c6b3307dfe0394bdea455cad95b76007183
Nav652 <82651142+Nav652@users.noreply.github.com>
2021-09-26 19:02:21 +0100

    Update Post_Login_UnSuccessful.java

--- src/test/java/com/API_Automation/testcases/Post_Login_UnSuccessful.java
+++ src/test/java/com/API_Automation/testcases/Post_Login_UnSuccessful.java
@@ -2,6 +2,7 @@ package com.API_Automation.testcases;
 
 import com.API_Automation.config.APIPath;
 import com.API_Automation.config.BaseTest;
+import com.API_Automation.config.payload;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
@@ -10,29 +11,44 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import static com.API_Automation.config.APIPath.POST_LOGIN_SUCCESSFUL;
+import static com.API_Automation.config.APIPath.POST_LOGIN_UNSUCCESSFUL;
+import static io.restassured.RestAssured.given;
+
 public class Post_Login_UnSuccessful extends BaseTest {
 
     private static RequestSpecification requestSpecification;
     private static Response response;
     /**
-     * API Post Request : Convert the response JSON body to Java Object
+     * API Post UnsuccessfulRequest : Convert the response JSON body to Java Object
      * This method will run before any test case and is dependent on BaseTest.setup
      */ 
     @BeforeClass(dependsOnMethods = "setup")
-    public void postLoginSuccesful() {
-        requestSpecification = RestAssured.given();
-        response = requestSpecification.request(Method.POST, APIPath.POST_LOGIN_UNSUCCESSFUL);
-        System.out.println(response.getBody().asString().toString());
+    @Test
+    public void postLoginUnSuccessful() {
+        response=given()
+                .header("Content-Type","application/json")
+                .body(payload.Post_Login_UnSuccessful())
+                .when()
+                .post(POST_LOGIN_UNSUCCESSFUL)
+                .then()
+                .extract().response();
 
     }
 
-    /**
-     * Validate GET Response StatusCode
-     */
-    @Test(priority = 0)   //This test will run first to confirm the successful GET call.
-    public void getAllUsers_StatusCode() {
-        Assert.assertEquals(response.getStatusCode(), 200
-                , "Get Request Response code mismatch -->");
+    @Test(priority = 0)   //This test will run first to confirm the successful POST call.
+    public void postLoginUnsuccessful_StatusCode() {
+        Assert.assertEquals(response.getStatusCode(), 400
+                , "Post Request Response code mismatch -->");
+        System.out.println(response.getBody().asString());
     }
+    @Test
+    public void postloginUnsuccessful_Body_Validation(){
+        Assert.assertEquals(response.getBody().asString(), payload.postResponseUnsuccessful());
+    }
+
+
 
 }
+
+
