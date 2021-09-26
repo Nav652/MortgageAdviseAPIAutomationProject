61dc1e6df9d1ae0db74a97214575f64c5e047acc
Nav652 <82651142+Nav652@users.noreply.github.com>
2021-09-26 18:54:28 +0100

    Update ExtentManager.java

--- src/test/java/com/API_Automation/utlils/ExtentManager.java
+++ src/test/java/com/API_Automation/utlils/ExtentManager.java
@@ -1,2 +1,37 @@
-package com.API_Automation.utlils;public class ExtentManager {
+package com.API_Automation.utlils;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+/**
+ * This class will initialise the Extent Test Report instance.
+ */
+public class ExtentManager {
+    private static ExtentReports extentReports;
+    private static final String reportFileName = "API_Testing_report.html";
+    private static final String macPath = System.getProperty("user.dir") + "/reports";
+    private static final String windowsPath = System.getProperty("user.dir") + "\\reports";
+    private static final String macReportFileLoc = macPath + "/" + reportFileName;
+    private static final String winReportFileLoc = windowsPath + "/" + reportFileName;
+
+    public static ExtentReports getInstance() {
+        if (extentReports == null)
+            createInstance();
+        return extentReports;
+    }
+
+    /**
+     * @return the Report instance with given configurations.
+     */
+    public static ExtentReports createInstance() {
+        ExtentSparkReporter sparkReporter = new ExtentSparkReporter(macReportFileLoc);
+        sparkReporter.config().setDocumentTitle("API Testing Report");
+        sparkReporter.config().setReportName(" API gggg");
+        sparkReporter.config().setTheme(Theme.DARK);
+        sparkReporter.config().setEncoding("utf-8");
+        extentReports = new ExtentReports();
+        extentReports.attachReporter(sparkReporter);
+        return extentReports;
+    }
 }
